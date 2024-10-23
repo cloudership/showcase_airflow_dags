c6dde7cb242c09ea93b445a3719e1ebc677f8220
Asfand Qazi <ayqazi@gmail.com>
2024-10-23 22:27:43 +0100

    ukpl venv imports fix

--- fetch_fixturedownload_football_uk_premier_league.py
+++ fetch_fixturedownload_football_uk_premier_league.py
@@ -1,15 +1,12 @@
 import logging
-from datetime import datetime, UTC
 
 import pendulum
-import requests
 from airflow.decorators import dag, task
 from airflow.io.path import ObjectStoragePath
-from airflow.models import Variable
 
 
 @dag(schedule=None,
-     start_date=datetime(2024, 1, 1, tzinfo=UTC),
+     start_date=pendulum.datetime(2024, 1, 1, tz="UTC"),
      description="Download UK Premier League football fixtures data",
      catchup=False)
 def fetch_fixturedownload_football_uk_premier_league():
@@ -22,13 +19,20 @@ def fetch_fixturedownload_football_uk_premier_league():
                          "requests",
                      ])
     def fetch(logical_date: pendulum.DateTime) -> ObjectStoragePath:
+        import logging
+        import requests
+        from airflow.io.path import ObjectStoragePath
+        from airflow.models import Variable
+
         endpoint = f"https://fixturedownload.com/feed/json/epl-{logical_date.year}"
         # This variable should contain the S3 path to store NYC taxi trip downloads; it should include the connection ID like:
         # "s3://<connection_id>@example-bucket/<path>/"
         bucket_root = ObjectStoragePath(Variable.get("s3_donotdrivenow_root"))
         path = bucket_root / f"{logical_date.year}/football/uk/premier_league-{logical_date.to_iso8601_string()}.json"
+        logging.info(f"Fetching {endpoint}")
         with requests.get(endpoint) as response:
             response.raise_for_status()
+            logging.info(f"Writing {endpoint} to {path}")
             with path.open("wb") as file:
                 file.write(response.content)
         return path
