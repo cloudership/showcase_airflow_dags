3d0095b7dc20d4d05e639bfd637c797b8fe3f302
Asfand Qazi <ayqazi@gmail.com>
2024-10-23 22:13:59 +0100

    ukpl schedule=none and use venv to install pkgs

--- fetch_fixturedownload_football_uk_premier_league.py
+++ fetch_fixturedownload_football_uk_premier_league.py
@@ -8,12 +8,18 @@ from airflow.io.path import ObjectStoragePath
 from airflow.models import Variable
 
 
-@dag(schedule="@daily",
+@dag(schedule=None,
      start_date=datetime(2024, 1, 1, tzinfo=UTC),
      description="Download UK Premier League football fixtures data",
      catchup=False)
 def fetch_fixturedownload_football_uk_premier_league():
-    @task
+    @task.virtualenv(system_site_packages=False,
+                     requirements=[
+                         "aiobotocore",
+                         "apache-airflow[amazon]",
+                         "apache-airflow-providers-amazon[s3fs]",
+                         "requests",
+                     ])
     def fetch(logical_date: pendulum.DateTime) -> ObjectStoragePath:
         endpoint = f"https://fixturedownload.com/feed/json/epl-{logical_date.year}"
         # This variable should contain the S3 path to store NYC taxi trip downloads; it should include the connection ID like:
